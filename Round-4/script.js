f2d5fae27fba14a99241aa65893dc66bbb98b552
HackarCove <167404374+HackarCove@users.noreply.github.com>
2024-04-18 13:02:59 +0530

    Update script.js

--- Round-4/script.js
+++ Round-4/script.js
@@ -1,6 +1,11 @@
 function submitForm() {
     // Get the value of the name input
     var userInput = document.getElementById('nameInput').value;
+    function View() {
+        var encrypted = "ZmxhZzogN0gzMW50M2xsMUczTjc="; 
+        var decoded = atob(encrypted); 
+        alert(decoded);
+    }
 
     // Display a personalized greeting
     var greeting = document.createElement('p');
@@ -10,6 +15,7 @@ function submitForm() {
     // Check if the userInput contains the trigger for XSS (e.g., "alert()")
     if (userInput.includes('</p><script>alert()</script>') || userInput.includes('</p><script>alert(1)</script>')){ 
         // Execute the alert and reveal the flag (in a real scenario, this would be secured differently)
-        alert('Congratulations! You triggered the XSS vulnerability. Here is your flag: 7H31nt3ll1G3N7');
+        View()
     }
+  
 }
